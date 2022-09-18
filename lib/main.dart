dbbf4fd0ed02450ac5e67a8b778211a787c33c57
prtham312 <98638038+prtham312@users.noreply.github.com>
2022-09-18 18:05:50 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Dayzer',
-      home: Page2(),
+      home: Page1(),
     );
   }
 }
